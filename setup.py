e977c06a03c9c7aea4e5cad1cf8cc083ddef20f9
mihagrgicjelen <miha.grgic.jelen@gmail.com>
2018-09-28 13:27:53 +0200

    vup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py_constant',
-    version='0.3dev',
+    version='0.4dev',
     packages=['py_constant',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
